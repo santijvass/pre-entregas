4f1d66ee502db926615fbc435183428f93423602
santiago vassallo <santiagojvassallo@gmail.com>
2023-04-25 22:30:36 -0300

    segundo.

---
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ItemDetail = ({ product }) => {
+  return (
+    <div>
+      <h2>product.title</h2>
+      <h2>product.description</h2>
+    </div>
+  );
+};
+
+export default ItemDetail;
